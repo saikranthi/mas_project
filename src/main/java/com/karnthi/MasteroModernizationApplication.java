b417b2fa78d325d5475d445e9485bb602efe0fe1
sai <sai@192.168.0.7>
2016-09-27 19:54:35 -0500

    initial commit

---
+++ src/main/java/com/karnthi/MasteroModernizationApplication.java
@@ -0,0 +1,12 @@
+package com.karnthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteroModernizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteroModernizationApplication.class, args);
+	}
+}
